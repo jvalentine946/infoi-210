e36d7919df8298cc4ac8e617fc40af625e19a2de
Jules Page <repage@iu.edu>
2025-03-04 10:20:24 -0500

    began homework 3, looping the DOM

---
+++ homework-3/js/lessons.js
@@ -0,0 +1,15 @@
+
+const instruments = [
+  "Piano",
+  "Guitar",
+  "Violin",
+  "Drums",
+  "Saxophone",
+  "Flute",
+];
+
+
+for (let i = 0; i < instruments.length; i++) {
+  const instruments = instruments[i];
+  ulRef.innerHTML += "<li class='item' >" + Lessons + "</li>";
+}
---
+++ homework-3/js/scores.js
@@ -0,0 +1,13 @@
+let scores = [95,
+    72,
+    88,
+    45,
+    60,
+    79,
+    82,
+    91,
+    50,
+    38,];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
